38c68293e2569e7873f58ec918b91dcfff13245d
Sergio Delgado <sergio@dgtl.ec>
2022-03-11 17:56:08 -0500

    pwj 01

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ console.log("hello");
 
 //alert('BINGO!')
 
+/*
 const a = 'smoothie';
 console.log(a);
 
@@ -11,3 +12,28 @@ console.log(someNumber);
 const age = prompt('What is your age?');
 
 document.getElementById('title').innerHTML = age;
+*/
+
+// Numbers
+
+var num1 = 25;
+
+// Increment by 1
+num1 += 1;
+num1++;
+
+console.log(num1);
+
+// Decrement by 1
+num1 -= 1;
+num1--;
+console.log(num1);
+
+// Divide, multiply, remainder %
+console.log(num1 * 6);
+console.log(num1 / 6);
+console.log(num1 % 6);
+
+// Increment/decrement by 11
+num1 += 10;
+console.log(num1);
