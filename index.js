c440792c20be7dee5fc8a33c5a9b59e62c6638bc
Sergio Delgado <sergio@dgtl.ec>
2022-03-11 23:38:07 -0500

    pwj 01

--- index.js
+++ index.js
@@ -37,3 +37,32 @@ console.log(num1 % 6);
 // Increment/decrement by 11
 num1 += 10;
 console.log(num1);
+
+// Functions
+// 1. Create a function
+
+function fun() {
+    console.log('this is a function');
+}
+
+// Call function
+fun();
+
+// Create a function that takes in a name and says hello followed by your name
+var name = prompt('What is your name?');
+
+function greeting (yourName) {
+    var result = 'Hello' + ' ' + yourName;
+    console.log(result);
+}
+
+greeting(name);
+
+function someNumbers(num1,num2) {
+    var result = num1 + num2;
+    console.log(result);
+}
+
+someNumbers(12, 15);
+someNumbers('12', 15);
+someNumbers('12', '15');
